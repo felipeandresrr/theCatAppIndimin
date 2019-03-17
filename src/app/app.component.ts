346cb612d44d376d859fe8a411bcb71f33b7bd62
Recabarren, Felipe <felipe.recabarren@accenture.com>
2019-03-17 19:46:39 -0300

    add apk and color status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,9 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      // statusBar.styleDefault();
+      statusBar.overlaysWebView(false);
+      statusBar.backgroundColorByHexString('#3E007F');
       splashScreen.hide();
     });
   }
